467eb951a9efabfd151bb31b908bed10bb9c93b4
hxchua00 <hxchua00@gmail.com>
2021-10-25 09:27:04 +0800

    Add project files.

---
+++ Q3_Prelims_RemoveDupeChar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_Prelims_RemoveDupeChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter string here: ");
+            //string text = Console.ReadLine();
+
+            string text = "The name is Cuttack whcich is near the Capital of State Odisha";
+
+            string[] substring = text.Split(' ');
+
+            for (int i =0; i< substring.Length; i++)
+            {
+                string temp = substring[i];
+                string result = "";
+
+                foreach (char value in temp)
+                {
+                    if (result.IndexOf(value) == -1)
+                    {
+                        result += value;
+                    }
+                }
+                substring[i] = result;
+            }
+
+            for (int i = 0; i < substring.Length; i++)
+            {
+                Console.Write(substring[i] + " ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
